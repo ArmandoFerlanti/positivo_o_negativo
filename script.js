72fa04d45461ad7f21ab52df1dacaa5b92ca0f18
Armando Ferlanti <armandoferlanti@outlook.it>
2024-04-04 10:56:45 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+document.getElementById("bottone").addEventListener("click", function() {
+    const inputNumero = document.getElementById("numero");
+    const numeroInserito = parseInt(inputNumero.value);
+
+    if (!isNaN(numeroInserito)) {
+        let risultato;
+        if (numeroInserito % 2 === 0) {
+            risultato = "Il numero inserito è pari";
+        } else {
+            risultato = "Il numero inserito è dispari";
+        }
+        document.getElementById("risultato").textContent = risultato;
+        document.getElementById("avviso").textContent = ""; 
+        document.getElementById("modal").style.display = "block";
+    } else {
+        document.getElementById("risultato").textContent = ""; 
+        document.getElementById("avviso").textContent = "il carattere inserito non è un numero valido."; 
+        document.getElementById("modal").style.display = "block";
+    }
+});
+
+
+document.getElementById("numero").addEventListener("keydown", function(event) {
+   
+    if (event.keyCode === 13) {
+        
+        event.preventDefault();
+        
+        
+        document.getElementById("bottone").click();
+    }
+});
+
+
+document.querySelector(".close").addEventListener("click", function() {
+    document.getElementById("modal").style.display = "none";
+});
+
+
+window.addEventListener("click", function(event) {
+    if (event.target == document.getElementById("modal")) {
+        document.getElementById("modal").style.display = "none";
+    }
+});
